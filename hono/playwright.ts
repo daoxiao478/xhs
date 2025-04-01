0df2c3bde83d3b9fa8d99ab8fe7c4886459b35fe
Atom <929746602@qq.com>
2025-04-01 09:14:47 +0800

    close local proxy

--- hono/playwright.ts
+++ hono/playwright.ts
@@ -5,7 +5,7 @@ import { DefaultApi } from "xhs-api-sdk";
 import tunnel from "tunnel";
 import _ from "lodash";
 
-const ENABLE_PROXY = true;
+const ENABLE_PROXY = false;
 const UAs = [
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
   "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
